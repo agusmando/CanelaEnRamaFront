e07e45503efa4d79a840db6c86b2ff64419c09bf
Agustín Mandolesi <agustinm@itcsoluciones.com>
2025-12-03 12:53:05 -0300

    Pequeños cambios para probar el funcionamiento de supertokens

--- src/Dashboard/index.tsx
+++ src/Dashboard/index.tsx
@@ -4,6 +4,25 @@ import {
 } from "supertokens-auth-react/recipe/session";
 import { getApiDomain } from "../config";
 import { useNavigate } from "react-router-dom";
+import Session from "supertokens-auth-react/recipe/session";
+
+async function getJWT() {
+  if (await Session.doesSessionExist()) {
+    let userId = await Session.getUserId();
+    let jwt = await Session.getAccessToken();
+    console.log(userId, jwt);
+  }
+}
+
+async function callApi() {
+  try {
+    const response = await fetch("http://localhost:8080/getJWT");
+    const data = await response.json();
+    window.alert("Session Information:\n" + JSON.stringify(data, null, 2));
+  } catch (err) {
+    window.alert("Error calling API: " + err.message);
+  }
+}
 
 export default function Dashboard() {
   const navigate = useNavigate();
@@ -21,7 +40,7 @@ export default function Dashboard() {
 
   async function logoutClicked() {
     await signOut();
-    navigate("/login");
+    navigate("/auth");
   }
 
   return (
@@ -41,7 +60,10 @@ export default function Dashboard() {
             {sessionContext.userId}
           </div>
           <div className="buttons">
-            <button onClick={callAPIClicked} className="dashboard-button">
+            <button onClick={getJWT} className="dashboard-button">
+              check JWT
+            </button>
+            <button onClick={callApi} className="dashboard-button">
               Call API
             </button>
             <button onClick={logoutClicked} className="dashboard-button">
